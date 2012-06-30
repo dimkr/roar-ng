5b9bcd338b59c0a0e4a2e79b13ad261c04b1195d
Dima Krasner <dima@dimakrasner.com>
2012-06-30 12:15:51 +0000

    Updated libbsd to version 0.4.2

--- skeleton/devx/build_scripts/var/packages/libbsd.sh
+++ skeleton/devx/build_scripts/var/packages/libbsd.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 PKG_NAME="libbsd"
-PKG_VER="0.3.0"
+PKG_VER="0.4.2"
 PKG_REV="1"
 PKG_DESC="BSD functions library"
 PKG_CAT="BuildingBlock"
@@ -23,11 +23,9 @@ build() {
 	cd $PKG_NAME-$PKG_VER
 
 	# configure the package
-	sed -e s~'^prefix.*=.*'~"prefix=/$BASE_INSTALL_PREFIX"~ \
-	    -e s~'^exec_prefix.*=.*'~"exec_prefix=/$BASE_INSTALL_PREFIX"~ \
-	    -e s~'^libdir.*=.*'~"libdir=/$LIB_DIR"~ \
-	    -e s~'^usrlibdir.*=.*'~"usrlibdir=/$LIB_DIR"~ \
-	    -i Makefile
+	./configure $AUTOTOOLS_BASE_OPTS \
+	            --disable-static \
+	            --enable-shared
 	[ $? -ne 0 ] && return 1
 
 	# build the package
@@ -40,10 +38,6 @@ build() {
 package() {
 	# install the package
 	make DESTDIR=$INSTALL_DIR install
-	[ $? -ne 0 ] && return 1
-
-	# remove the static library, to prevent packages from linking against it
-	rm -f $INSTALL_DIR/$LIB_DIR/libbsd.a
 	[ $? -ne 0 ] && return 1
 
 	# install the license
