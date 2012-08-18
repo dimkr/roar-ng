32fd0cf691255042755df01b9f6915acd715667b
Dima Krasner <dima@dimakrasner.com>
2012-08-18 15:53:36 +0000

    Reverted back to the latest mplayer2 from git, as the old version still depends on Python 3.
    It seems that mplayer2 had a Python 2-compatible script. However, once other scripts were added, it was ported to Python 3 syntax as well. Currently, there is no clean way to avoid this dependency.

--- skeleton/devx/build_scripts/var/packages/mplayer2.sh
+++ skeleton/devx/build_scripts/var/packages/mplayer2.sh
@@ -1,26 +1,33 @@
 #!/bin/sh
 
 PKG_NAME="mplayer2"
-PKG_VER="git160712"
+PKG_VER="git$(date +%d%m%Y)"
 PKG_REV="1"
 PKG_DESC="Video player forked from MPlayer"
 PKG_CAT="Multimedia"
 PKG_DEPS="+ncurses,+libav"
 
-# the commit
-PKG_COMMIT="86571435baf116a91a31451d26224600f3575270"
-
 download() {
-	[ -f $PKG_NAME-$PKG_VER.tar.bz2 ] && return 0
+	[ -f $PKG_NAME-$PKG_VER.tar.xz ] && return 0
+
 	# download the sources
-	download_file http://git.mplayer2.org/mplayer2/snapshot/$PKG_NAME-$PKG_COMMIT.tar.bz2
+	git clone --depth 1 git://git.mplayer2.org/mplayer2.git $PKG_NAME-$PKG_VER
 	[ $? -ne 0 ] && return 1
+
+	# create a sources tarball
+	tar -c $PKG_NAME-$PKG_VER | xz -9 -e > $PKG_NAME-$PKG_VER.tar.xz
+	[ $? -ne 0 ] && return 1
+
+	# clean up
+	rm -rf $PKG_NAME-$PKG_VER
+	[ $? -ne 0 ] && return 1
+
 	return 0
 }
 
 build() {
 	# extract the sources tarball
-	tar -xjvf $PKG_NAME-$PKG_VER.tar.bz2
+	tar -xJvf $PKG_NAME-$PKG_VER.tar.xz
 	[ $? -ne 0 ] && return 1
 
 	cd $PKG_NAME-$PKG_VER
