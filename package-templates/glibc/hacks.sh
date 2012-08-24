2475466424d6780f67dd9232ccccbd886ae6b2c7
Dima Krasner <dima@dimakrasner.com>
2012-08-24 11:38:23 +0000

    Added /usr/lib/locale, a symlink to /usr/lib64/locale

--- package-templates/glibc/hacks.sh
+++ package-templates/glibc/hacks.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+# on 64-bit systems, link /usr/lib/locale to /usr/lib64/locale
+if [ -d ./usr/lib64/locale ] && [ ! -e ./usr/lib/locale ]
+then
+	ln -s ../lib64/locale ./usr/lib/locale
+fi
+
 # remove all generated locales
-if [ -d ./usr/lib$LIBDIR_SUFFIX/locale ]
+if [ -e ./usr/lib/locale ]
 then
 	echo "    removing generated locales"
-	rm -rf ./usr/lib$LIBDIR_SUFFIX/locale/*
+	rm -rf ./usr/lib/locale/*
 fi
